c6a927ce307ffeb3c0952027687bca36575a29de
Marina Zaytseva <zaytseva@surf.dev>
2026-04-09 23:15:54 +0300

    feat(ui): добавлен press state для DivoBrandButton по паттерну SolidRoundedButtonNode

--- submodules/OnboardingUI/Sources/DivoBrandButton.swift
+++ submodules/OnboardingUI/Sources/DivoBrandButton.swift
@@ -13,6 +13,18 @@ public final class DivoBrandButton: UIButton {
         fatalError("init(coder:) has not been implemented")
     }
 
+    public override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                layer.removeAnimation(forKey: "opacity")
+                alpha = 0.55
+            } else {
+                alpha = 1.0
+                layer.animateAlpha(from: 0.55, to: 1.0, duration: 0.2)
+            }
+        }
+    }
+
     private func setup() {
         setTitleColor(.white, for: .normal)
         setTitleColor(DivoColors.disabledText, for: .disabled)
