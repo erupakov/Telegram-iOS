c6329b783af1098411740c68e67918c111f65269
ShagMichail <shiga2001@mail.ru>
2026-06-01 15:20:49 +0300

    feat(ui) : добавлено форматирование цен + форматирование цены при создании/редактировании эвента

--- submodules/EventsUI/Sources/EventDetail/EventDetailControllerNode.swift
+++ submodules/EventsUI/Sources/EventDetail/EventDetailControllerNode.swift
@@ -1764,6 +1764,9 @@ final class EventDetailControllerNode: ASDisplayNode {
         formatter.roundingMode = .halfUp
         formatter.decimalSeparator = "."
         
+        formatter.usesGroupingSeparator = true
+        formatter.groupingSeparator = "\u{00a0}"
+        
         return formatter.string(from: NSNumber(value: doubleValue))
     }
     
